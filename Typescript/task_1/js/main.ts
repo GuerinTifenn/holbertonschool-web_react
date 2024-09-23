fe61701d7fe4fd2110b96bf0e766efb10160d540
GuerinTifenn <8807@holbertonstudents.com>
2024-09-23 14:16:38 +0200

    Add all project

---
+++ Typescript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+const teacher1: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  contract: false
+};
+
+console.log(teacher1);
---
+++ Typescript/task_4/js/main.ts
@@ -0,0 +1,27 @@
+namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+    experienceTeachingC?: number;
+  }
+
+  export class Subject {
+    teacher: Teacher;
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (!this.teacher.experienceTeachingC) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}
